1a632827a66c97b5087967492cdb5ef5978ef63f
Vinícius Lima <vinislima@gmail.com>
2025-03-28 20:33:19 +0000

    estudos

--- exam_rank_02/level_01/repeat_alpha.c
+++ exam_rank_02/level_01/repeat_alpha.c
@@ -6,7 +6,7 @@
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 18:28:56 by vinda-si          #+#    #+#             */
-/*   Updated: 2025/03/27 18:35:41 by vinda-si         ###   ########.fr       */
+/*   Updated: 2025/03/28 17:43:05 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 int	main(void)
 {
 	int	i;
-	int j;
-	int z;
-	char *word = "teste";
+	int dif;
+	int calc;
+	char word[] = "teste";
 
 	i = 0;
 	while (word[i] != '\0')
 	{
-		j = 0;
+		dif = 0;
+		calc = 0;
 		if (word[i] >= 'a' && word[i]<= 'z')
-			while (j <= )
+		{
+			dif = word[i] - 66;
+			while (dif >= )
+		}
 	}
 }
--- exam_rank_02/level_01/search_and_replace.c
+++ exam_rank_02/level_01/search_and_replace.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ulstr.c                                            :+:      :+:    :+:   */
+/*   search_and_replace.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/27 19:58:34 by vinda-si          #+#    #+#             */
-/*   Updated: 2025/03/28 18:03:37 by vinda-si         ###   ########.fr       */
+/*   Created: 2025/03/28 18:24:38 by vinda-si          #+#    #+#             */
+/*   Updated: 2025/03/28 20:07:25 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,35 @@
 
 int	main(int argc, char *argv[])
 {
-	if (argc != 2)
+	int i;
+
+	if (argc != 4)
 	{
 		write(1, "\n", 1);
 		return (0);
 	}
-	
-	int i = 0;
+	if (argv[2][1] != '\0' || argv[3][1] != '\0')
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	if ((argv[2][0] >= 91 && argv[2][0] <= 96) || argv[2][0] <= 64 || argv[2][0] >= 123)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	if ((argv[3][0] >= 91 && argv[3][0] <= 96) || argv[3][0] <= 64 || argv[3][0] >= 123)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+
+	i = 0;
 	while (argv[1][i] != '\0')
 	{
-		if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
-		{
-			argv[1][i] = argv[1][i] - 32;
-			write(1, &argv[1][i], 1);
-		}
-		else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-		{
-			argv[1][i] = argv[1][i] + 32;
-			write(1, &argv[1][i], 1);
-		}
-		else
-			write(1, &argv[1][i], 1);
+		if (argv[1][i] == argv[2][0])
+			argv[1][i] = argv[3][0];
+		write(1, &argv[1][i], 1);
 		i++;
 	}
 	write(1, "\n", 1);
