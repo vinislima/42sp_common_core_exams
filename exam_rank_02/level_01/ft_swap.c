d8540c9e1b2cc03c157ecc7f4a800f842748c821
Vinícius Lima <vinislima@gmail.com>
2025-03-27 23:34:03 -0300

    estudo

--- exam_rank_02/level_01/ft_swap.c
+++ exam_rank_02/level_01/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 22:04:41 by vinda-si          #+#    #+#             */
-/*   Updated: 2025/03/27 18:21:08 by vinda-si         ###   ########.fr       */
+/*   Updated: 2025/03/27 21:44:08 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ void	ft_swap(int *a, int *b)
 // 	int	*b;
 // 	int	numb_a;
 // 	int	numb_b;
-//
+
 // 	numb_a = 10;
 // 	numb_b = 100;
-//
+
 // 	a = &numb_a;
 // 	b = &numb_b;
-//	
+	
 // 	printf("A = %u, B = %u\n", *a, *b);
 // 	ft_swap(a, b);
 // 	printf("A = %u, B = %u\n", *a, *b);
--- exam_rank_02/level_01/ulstr.c
+++ exam_rank_02/level_01/ulstr.c
@@ -1,32 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   search_and_replace.c                               :+:      :+:    :+:   */
+/*   ulstr.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 19:58:34 by vinda-si          #+#    #+#             */
-/*   Updated: 2025/03/27 20:43:45 by vinda-si         ###   ########.fr       */
+/*   Updated: 2025/03/27 23:32:56 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	int i = 0;
-	char *str = "tEsTe";
-
-	while (str[i] != '\0')
+	if (argc != 2)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		write(1, "\n", 1);
+		return (0);
+	}
+	
+	int i = 0;
+	while (argv[1][i] != '\0')
+	{
+		if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
 		{
-			i -= 'a';
-			write()
-		}	
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 'A';
-		write(1, &str[i], 1);
+			argv[1][i] = argv[1][i] - 32;
+			write(1, &argv[1][i], 1);
+		}
+		else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+		{
+			argv[1][i] = argv[1][i] + 32;
+			write(1, &argv[1][i], 1);
+		}
 		i++;
 	}
 	write(1, "\n", 1);
