2fad67950ab2922b44f1e0a47e75d008bec04036
Vinícius Lima <vinislima@gmail.com>
2025-03-30 23:54:12 -0300

    estudo

--- exam_rank_02/level_01/repeat_alpha.c
+++ exam_rank_02/level_01/repeat_alpha.c
@@ -6,28 +6,41 @@
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 18:28:56 by vinda-si          #+#    #+#             */
-/*   Updated: 2025/03/28 17:43:05 by vinda-si         ###   ########.fr       */
+/*   Updated: 2025/03/30 22:28:17 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
 	int	i;
-	int dif;
-	int calc;
-	char word[] = "teste";
+	int j;
+	char *word;
+
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
 
 	i = 0;
+	j = 1;
+	word = argv[1];
 	while (word[i] != '\0')
 	{
-		dif = 0;
-		calc = 0;
-		if (word[i] >= 'a' && word[i]<= 'z')
+		j = 1;
+		if (word[i] >= 'A' && word[i] <= 'Z')
+			j = word[i] - 64;
+		if (word[i] >= 'a' && word[i] <= 'z')
+			j = word[i] - 96;
+		while (j >= 1)
 		{
-			dif = word[i] - 66;
-			while (dif >= )
+			write(1, &word[i], 1);
+			j--;
 		}
+		i++;
 	}
+	write(1, "\n", 1);
+	return (0);
 }
