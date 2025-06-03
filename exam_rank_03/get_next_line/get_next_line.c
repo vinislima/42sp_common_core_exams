8d0ac608c34594fc182686a5b487ad382ffc713c
Vinícius Lima <vinislima@gmail.com>
2025-06-03 19:41:22 +0000

    estudo

--- exam_rank_03/get_next_line/get_next_line.c
+++ exam_rank_03/get_next_line/get_next_line.c
@@ -6,7 +6,57 @@
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 18:29:31 by vinda-si          #+#    #+#             */
-/*   Updated: 2025/06/03 16:49:17 by vinda-si         ###   ########.fr       */
+/*   Updated: 2025/06/03 19:41:10 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strdup(char *str)
+{
+	int		i = 0;
+	char	*dest;
+
+	while (str[i])
+		i++;
+
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+
+	i = 0;
+	while (str[i])
+	{
+		dest[i] = str[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE];
+	char		line[70000];
+	static int	pos;
+	static int	size_read;
+	int			i = 0;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (1)
+	{
+		if (pos >= size_read)
+		{
+			size_read = read(fd, buffer, BUFFER_SIZE);
+			pos = 0;
+			if (size_read == 0)
+				break ;
+		}
+		line[i++] = buffer[pos++];
+		if (buffer[pos] == '\n')
+			break ;
+	}
+	line[i] = '\0';
+	if (i == 0)
+		return (NULL);
+	return (ft_strdup(line));
+}
---
+++ exam_rank_03/get_next_line/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 19:00:52 by vinda-si          #+#    #+#             */
+/*   Updated: 2025/06/03 19:07:51 by vinda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+char	*get_next_line(int fd);
+
+#endif
